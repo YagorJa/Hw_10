69bc817b4b2973627872b75808eebfd2308a82f2
Yahor \|/ <yagorpraca@gmail.com>
2023-12-04 18:11:49 +0300

    1 часть

---
+++ src/Sistema.java
@@ -0,0 +1,29 @@
+import java.util.Objects;
+
+public class Sistema {
+ private String name;
+ private int chislo;
+ private String string;
+
+    public Sistema(String name, int chislo, String string) {
+        this.name = name;
+        this.chislo = chislo;
+        this.string = string;
+    }
+    public boolean equals(Object o) {
+       // сказано же просто переопределить
+        // не сказано как
+        if (this == o) {
+            return true;
+        }
+        Sistema sistema = (Sistema) o;
+        return name.equals(sistema.name);
+    }
+    public int hashCode(){
+        return Objects.hash(name,chislo,string);
+    }
+
+    public String toString() {
+        return chislo + "," + string + " " + name;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test{
+    public static void main(String[] args) {
+        Sistema sistema = new Sistema("SystemOfADown",8,"not cool");
+        Sistema sistema1 = new Sistema("dk",7,"hg");
+        Sistema sistema2 = new Sistema("SystemOfADown",8,"not cool");
+        System.out.println(sistema.toString());
+        System.out.println(sistema.equals(sistema2));
+    }
+}
